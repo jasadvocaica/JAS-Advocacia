ab48404403f832f2572d1f0bfc3d8137004989a1
jasadvocaica <clientesjulianaaraujo@gmail.com>
2026-09-11 08:44:13 -0400

    security(comercial): restringir conversão a autenticados

---
+++ supabase/migrations/20260911125900_restringir_conversao_conversa_autenticados.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.comercial_converter_conversa_em_lead(uuid) from anon;
+grant execute on function public.comercial_converter_conversa_em_lead(uuid) to authenticated;
